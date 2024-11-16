aedf26d83f604bdf7a3bcca90e8e5780660a687b
Anthony Manning-Franklin <anthony@skutopia.com>
2024-11-16 10:43:49 +0800

    chore(docs): missing docs update from previous release

--- lib/indexes/hash.d.ts
+++ lib/indexes/hash.d.ts
@@ -1,6 +1,6 @@
-import { IndexableObj, IndexOptions, Ingestor, Updater } from './common';
+import { Deleter, IndexableObj, IndexOptions, Ingestor, Updater } from './common';
 export declare type HashIndex<T extends IndexableObj> = Record<string, {
     get: (value: T[string]) => Set<T> | undefined;
 }>;
-export declare function createHashIndex<T extends IndexableObj>({ targetProperties, }: IndexOptions<T>): [HashIndex<T>, Updater<T>, Ingestor<T>];
+export declare function createHashIndex<T extends IndexableObj>({ targetProperties, }: IndexOptions<T>): [HashIndex<T>, Updater<T>, Ingestor<T>, Deleter<T>];
 //# sourceMappingURL=hash.d.ts.map
